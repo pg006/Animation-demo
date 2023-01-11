d1c6441ccf539337c3d80b98c05e04aeb42188f1
Milan Bhikadiya <milanbhikadiya22@gmail.com>
2023-01-11 11:24:40 +0530

    - Initial Commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import AllRoutes from "./Routes/AllRoutes";
+
+const App = () => {
+  return (
+    <React.Fragment>
+      <AllRoutes />
+    </React.Fragment>
+  );
+};
+
+export default App;
---
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Dashboard = () => {
+  return (
+    <React.Fragment>
+      <marquee>
+        <h1>Animation Demo</h1>
+      </marquee>
+    </React.Fragment>
+  );
+};
+
+export default Dashboard;
